35d712a19ee1c61fd1ea23ca57a5b57adf9e711e
Fathur Rohman <fathur_rohman17@yahoo.co.id>
2016-09-05 22:53:41 +0700

    model

---
+++ app/Models/Role.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akung
+ * Date: 9/5/16
+ * Time: 22:49
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+
+}
---
+++ app/Models/Route.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akung
+ * Date: 9/5/16
+ * Time: 22:50
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Route extends Model
+{
+
+}
